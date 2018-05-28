02bf60975116b42e4d399e76a158bd9bfc0ec5d6
freyahelstrup <s165232@student.dtu.dk>
2018-05-28 09:48:35 +0200

    Kommentar til status kode

--- Lektion 12/src/rest/InvalidIdMapper.java
+++ Lektion 12/src/rest/InvalidIdMapper.java
@@ -10,7 +10,7 @@ public class InvalidIdMapper implements ExceptionMapper<InvalidIdException> {
     @Override
     public Response toResponse(InvalidIdException exception) {
         return Response
-                .status(Response.Status.BAD_REQUEST)
+                .status(Response.Status.BAD_REQUEST) //Status kode 400
                 .entity(exception.getMessage())
                 .type(MediaType.TEXT_PLAIN)
                 .build();
