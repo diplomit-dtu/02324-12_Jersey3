5366294aec0da67ccb377c8f5e771a23da778723
freyahelstrup <s165232@student.dtu.dk>
2018-05-28 09:36:56 +0200

    Ændret exception og mapper til to klasser

--- Lektion 12/src/rest/InvalidIdException.java
+++ Lektion 12/src/rest/InvalidIdException.java
@@ -1,15 +1,7 @@
 package rest;
 
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
-import javax.ws.rs.ext.ExceptionMapper;
-import javax.ws.rs.ext.Provider;
-
 @SuppressWarnings("serial")
-@Provider
-public class InvalidIdException extends WebApplicationException implements ExceptionMapper<InvalidIdException> {
+public class InvalidIdException extends Exception  {
 
 	public InvalidIdException(){
 		super("ID er allerede i brug");
@@ -19,13 +11,5 @@ public class InvalidIdException extends WebApplicationException implements Excep
 		super(message);
 	}
 
-	@Override
-	public Response toResponse(InvalidIdException exception) {
-		return Response
-				.status(Status.BAD_REQUEST)
-				.entity(exception.getMessage())
-				.type(MediaType.TEXT_PLAIN)
-				.build();
-	}
 
 }
